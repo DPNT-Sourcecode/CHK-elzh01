c34bb4feb2f8201900d430c95ad54f9538245342
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-04-13 14:14:40 +0000

    Sun Apr 13 14:14:40 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -6,14 +6,34 @@ from lib.solutions.SUM.sum_solution import SumSolution
 class TestSum:
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+        assert SumSolution().compute(0, 0) == 0
+        assert SumSolution().compute(100, 100) == 200
+        assert SumSolution().compute(64, 37) == 101
 
     def test_x_validated_type(self):
         with pytest.raises(TypeError):
             assert SumSolution().compute("1", 2) == 3
 
     def test_x_validated_lower_bound(self):
+        with pytest.raises(ValueError):
+            assert SumSolution().compute(-1, 2) == 3
+
+    def test_x_validated_upper_bound(self):
+        with pytest.raises(ValueError):
+            assert SumSolution().compute(101, 2) == 3
+
+    def test_y_validated_type(self):
         with pytest.raises(TypeError):
-            assert SumSolution().compute("1", 2) == 3
+            assert SumSolution().compute(1, "2") == 3
+
+    def test_y_validated_lower_bound(self):
+        with pytest.raises(ValueError):
+            assert SumSolution().compute(1, -1) == 3
+
+    def test_y_validated_upper_bound(self):
+        with pytest.raises(ValueError):
+            assert SumSolution().compute(1, 101) == 3
+
 
 
 
