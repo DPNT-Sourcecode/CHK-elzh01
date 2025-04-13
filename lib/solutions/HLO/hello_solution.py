8ba40709ed5335cf2fa4fc335ea4914cbf99b26a
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-04-13 14:28:20 +0000

    Sun Apr 13 14:28:20 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,5 @@
 class HelloSolution:
     
     # friend_name = unicode string
-    def hello(self, friend_name: str) -> str:
+    def hello(self, friend_name) -> str:
         raise NotImplementedError()
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -5,9 +5,16 @@ from lib.solutions.HLO.hello_solution import HelloSolution
 
 class TestHello:
     def test_hello(self):
-        hello = HelloSolution()
+       assert HelloSolution().hello("abc") == "Hello Abc!"
 
 
     def test_friend_name_validated_type(self):
         with pytest.raises(TypeError):
             assert HelloSolution().hello(123)
+
+    def test_friend_name_validated_type_allow_printable(self):
+        class Printable:
+            def __str__(self):
+                return "xyz"
+
+        assert isinstance(HelloSolution().hello(Printable()), str)
