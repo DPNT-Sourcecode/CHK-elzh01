9444e326bbf2795758bcbdc557d3f8d8d5d557a7
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-04-13 15:13:53 +0000

    Sun Apr 13 15:13:53 UTC 2025

--- lib/solutions/CHK/repository.py
+++ lib/solutions/CHK/repository.py
@@ -5,8 +5,11 @@ class Repository:
         self.data = data
 
     def price_for(self, sku: str, amount: int) -> int:
-        sku_data = self.data[sku]
+        if sku not in self.data:
+            raise ValueError("Unknown SKU: {}".format(sku))
+        sku_data = self.data[sku].copy()
         price = 0
         while amount > 0:
-            pass
+            sku_data
         return price
+
