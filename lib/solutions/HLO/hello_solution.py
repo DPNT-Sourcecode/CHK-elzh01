f17eec0d7edbb05c8491b9df250f3e0f3580b6c6
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-04-13 14:31:20 +0000

    Sun Apr 13 14:31:20 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,7 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name) -> str:
-        raise NotImplementedError()
+        if isinstance(friend_name, str) or type(friend_name).__str__ is not object.__str__:
+            return format("Hello {}!", friend_name)
+        raise TypeError("The friend_name must be a str or explicitly implement __str__")
+
